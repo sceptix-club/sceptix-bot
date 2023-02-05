1a015b342483241f7ecb46ad13503b1b06411716
Vyasa M Nayak <21d61.vyasa@sjec.ac.in>
2023-02-05 12:36:35 +0530

    Added two games RPS and RPSSL

--- helpers.py
+++ helpers.py
@@ -1,7 +1,7 @@
 import discord
 
 
-def find_user(author, users, pat) -> discord.Member:
+def find_user(author: discord.Member, users: list, pat: str) -> discord.Member:
     pat = str(pat).lower()
     for u in users:
         if u.name.lower().startswith(pat):
@@ -11,3 +11,50 @@ def find_user(author, users, pat) -> discord.Member:
         if str(u.id) in pat:
             return u
     raise Exception('User Not Found')
+
+
+def get_rps(choice: str) -> str:
+    choice = choice.lower().strip()
+    if 'rock' in choice or 'r' == choice:
+        return "Rock"
+    if 'paper' in choice or 'p' == choice:
+        return "Paper"
+    if 'scissor' in choice or 's' == choice: # Scissor spelt singularly for better UX
+        return "Scissors"
+    raise Exception('Invalid Choice')
+
+
+def play_rps(my_choice: str, bot_choice: str) -> str:
+    if my_choice == "Rock" and bot_choice == "Scissors":
+        return "You Won!"
+    if my_choice == "Paper" and bot_choice == "Rock":
+        return "You Won!"
+    if my_choice == "Scissors" and bot_choice == "Paper":
+        return "You Won!"
+    if my_choice == bot_choice:
+        return "That's a tie!"
+    return "You lost \U0001F641"
+
+
+def get_rpssl(choice: str) -> int:
+    choice = choice.lower().strip()
+    if choice.startswith('r'): return 0
+    if choice.startswith('p'): return 1
+    if choice.startswith('sc'): return 2
+    if choice.startswith('sp'): return 3
+    if choice.startswith('l'): return 4
+    raise Exception('Invalid Choice')
+
+
+def play_rpssl(my_choice: int, bot_choice: int) -> str:
+    diff = abs(bot_choice - my_choice)
+    if diff == 0:
+        return "That's a tie!"
+    if diff in [1, 3]:
+        winner = max(bot_choice, my_choice)
+    if diff in [2, 4]:
+        winner = min(bot_choice, my_choice)
+    if winner == my_choice:
+        return "You won!"
+    else:
+        return "You lost \U0001F641"
--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from datetime import datetime
 from dotenv import load_dotenv
 from discord.ext import commands
 
-from helpers import find_user
+from helpers import find_user, get_rps, play_rps, get_rpssl, play_rpssl
 import messages as ms
 
 load_dotenv()
@@ -32,9 +32,10 @@ async def on_member_join(member):
 # COMMANDS
 
 @bot.command(name='av', description='Returns the avatar of the specified user')
-async def av(ctx, *, user=None):
+async def av(ctx, *, user: str=None):
     if user is None:
         await ctx.send(ctx.author.display_avatar)
+        return
     try:
         user = find_user(ctx.author, [u for u in ctx.guild.members], user)
     except Exception:
@@ -42,6 +43,7 @@ async def av(ctx, *, user=None):
     else:
         await ctx.send(user.display_avatar)
 
+# UTILITIES
 
 @bot.command(name='choose', description='Chooses between multiple choices')
 async def choose(ctx, *choices: str):
@@ -51,19 +53,6 @@ async def choose(ctx, *choices: str):
 @bot.command(name='flip', description='Flips a coin')
 async def flip(ctx):
     await ctx.send(random.choice(["Heads", "Tails"]))
-
-
-# @bot.command(name='repeat',description='Repeats a message numtiple times')
-# async def repeat(ctx, *, times: int=0, content=None):
-#     if not 1 <= times <= 10:
-#         await ctx.send("```!repeat {number-of-times} {content}\nRepeat 1 to 10 times only.```")
-#         return
-    
-#     if content is None:
-#         content = f'{ctx.author.name} is cool!'
-
-#     for i in range(times):
-#         await ctx.send(content)
 
 
 @bot.command(description='Shows when a member joined')
@@ -77,6 +66,39 @@ async def joined(ctx, member=None):
     else:
         await ctx.send(f'{member.display_name} joined {ctx.guild.name} on {discord.utils.format_dt(member.joined_at)}')
 
+# GAMES
+
+@bot.command(name='rps', description='Play Rock Paper Scissors')
+async def rps(ctx, *, choice: str=None):
+    if choice is None:
+        await ctx.send("```!rps {your-choice}\nRock (R), Paper (P), or Scissors (S).```")
+        return
+    try:
+        choice = get_rps(choice)
+    except Exception:
+        await ctx.send('Invalid Choice')
+    else:
+        bot_choice = random.choice(["Rock", "Paper", "Scissors"])
+        await ctx.send(f'I chose {bot_choice}')
+        await ctx.send(play_rps(choice, bot_choice))
+
+
+# Game inspired from this article
+# https://eduherminio.github.io/blog/rock-paper-scissors/
+@bot.command(name='rpssl', description='Play Rock Paper Scissors Spock Lizard')
+async def rpsls(ctx, *, choice: str=None):
+    if choice is None:
+        await ctx.send("```!rpsls {your-choice}\nRock (Ro), Paper (Pa), Scissors (Sc), Spock (Sp), and Lizard (Li).```")
+        return
+    try:
+        choice = get_rpssl(choice)
+    except Exception:
+        await ctx.send('Invalid Choice')
+    else:
+        bot_choice = random.randint(0, 4)
+        choices = ["Rock", "Paper", "Scissors", "Spock", "Lizard"]
+        await ctx.send(f'I chose {choices[bot_choice]}')
+        await ctx.send(play_rpssl(choice, bot_choice))
 
 # PRIVILEDGED COMMANDS
 
@@ -93,7 +115,6 @@ async def delete_messages(ctx, *, count: int=10):
 async def on_command_error(ctx, error):
     if isinstance(error, commands.errors.CheckFailure):
         await ctx.send('You are not a Council Member.')
-
 
 
 bot.run(TOKEN)
