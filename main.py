6b67f5d2ca359b8418aa46c442ce2802d7cc91c9
Vyasa M Nayak <21d61.vyasa@sjec.ac.in>
2023-02-05 18:46:25 +0530

    Bulk add/remove roles

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 import os
 import random
-import re
 import discord
-import typing
 from datetime import datetime
 from dotenv import load_dotenv
 from discord.ext import commands
 from discord.ext.commands import has_role, has_permissions
 
-from helpers import find_user, get_rps, play_rps, get_rpssl, play_rpssl
+from helpers import *
 import messages as ms
 
 load_dotenv()
@@ -21,6 +19,7 @@ bot = commands.Bot(command_prefix='!', intents=discord.Intents().all())
 
 @bot.event
 async def on_ready():
+    await bot.change_presence(activity=discord.Game('with your heart'))
     guild = discord.utils.get(bot.guilds, name=GUILD)
     print(f'{bot.user.name} is connected to {guild.name}, at ID: {guild.id}')
 
@@ -38,6 +37,7 @@ async def av(ctx, *, user: str=None):
         await ctx.send(ctx.author.display_avatar)
         return
     try:
+        # Used instead of Guild.get_member_named() for better UX
         user = find_user(ctx.author, [u for u in ctx.guild.members], user)
     except Exception:
         await ctx.send('Member not found')
@@ -67,6 +67,13 @@ async def joined(ctx, member=None):
     else:
         await ctx.send(f'{member.display_name} joined {ctx.guild.name} on {discord.utils.format_dt(member.joined_at)}')
 
+@bot.command(name='announce', description='Announces a message as an embed')
+@commands.has_role('The High Table')
+async def announce(ctx, channel: discord.TextChannel, heading: str, content: str):
+    embed = discord.Embed(title=heading, description=content, colour=0x48cae4)
+    embed.set_author(name=ctx.author.display_name, icon_url=ctx.author.display_avatar)
+    await channel.send(content='@everyone', embed=embed)
+
 # GAMES
 
 @bot.command(name='rps', description='Play Rock Paper Scissors')
@@ -81,7 +88,7 @@ async def rps(ctx, *, choice: str=None):
     else:
         bot_choice = random.choice(["Rock", "Paper", "Scissors"])
         await ctx.send(f'You chose {choice}')
-        await ctx.send(f'I chose {bot_choice}')
+        await ctx.send(f'I choose {bot_choice}')
         await ctx.send(play_rps(choice, bot_choice))
 
 
@@ -100,7 +107,7 @@ async def rpsls(ctx, *, choice: str=None):
         choices = ["Rock", "Paper", "Scissors", "Spock", "Lizard"]
         await ctx.send(f'You chose {choices[choice]}')
         bot_choice = random.randint(0, 4)
-        await ctx.send(f'I chose {choices[bot_choice]}')
+        await ctx.send(f'I choose {choices[bot_choice]}')
         await ctx.send(play_rpssl(choice, bot_choice))
 
 # PRIVILEDGED COMMANDS
@@ -119,6 +126,24 @@ async def delete_messages(ctx, *, count: int=10):
 async def kick(ctx, member: discord.Member, reason: str=None):
     await member.kick(reason=reason)
     await ctx.send(f"{member.display_name} has been kicked" + (reason) * f"for {reason}")
+
+
+@bot.command(name='roleadd', description='Adds a specified role to all the human members of the server')
+@commands.has_role('The High Table')
+async def roleadd(ctx, role: discord.Role):
+    for u in ctx.guild.members:
+        if not u.bot:
+            await u.add_roles(ctx.guild.get_role(role.id))
+    await ctx.send(f'{role.name} added')
+
+
+@bot.command(name='roleremove', description='Removes a specified role to all the human members of the server')
+@commands.has_role('The High Table')
+async def roleremove(ctx, role: discord.Role):
+    for u in ctx.guild.members:
+        if not u.bot:
+            await u.remove_roles(ctx.guild.get_role(role.id))
+    await ctx.send(f'{role.name} removed')
 
 # ERRORS
 
